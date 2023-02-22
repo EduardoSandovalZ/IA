a4921aa21155ec25cc4e66320e42772719f39eb4
EduardoSandovalZ <100109750+EduardoSandovalZ@users.noreply.github.com>
2023-02-21 23:07:38 -0600

    Update LAB4.py

--- LAB4/LAB4.py
+++ LAB4/LAB4.py
@@ -1,70 +1,44 @@
-# Unicode representation of the queen symbol
 queen = "\u2655"
-
-# Check if the position is available for placing the queen
-def is_available(board, row, col, n):
-    # Check column
-    for i in range(row):
-        if board[i][col] == queen:
-            return False
-
-    # Check upper left diagonal
-    i, j = row, col
-    while i >= 0 and j >= 0:
-        if board[i][j] == queen:
-            return False
-        i -= 1
-        j -= 1
-
-    # Check upper right diagonal
-    i, j = row, col
-    while i >= 0 and j < n:
-        if board[i][j] == queen:
-            return False
-        i -= 1
-        j += 1
-
-    return True
-
-# Place the queens on the board using depth-first search
+def is_available(board, row, column, n):
+   for i in range(row):
+       if board[i][column] == queen:
+           return False
+   i, j = row, column
+   while i >= 0 and j >= 0:
+       if board[i][j] == queen:
+           return False
+       i -= 1
+       j -= 1
+   i, j = row, column
+   while i >= 0 and j < n:
+       if board[i][j] == queen:
+           return False
+       i -= 1
+       j += 1
+   return True
 def place_queens(board, row, n):
-    if row == n:
-        return True
-
-    for col in range(n):
-        if is_available(board, row, col, n):
-            board[row][col] = queen
-
-            if place_queens(board, row + 1, n):
-                return True
-
-            board[row][col] = 0
-
-    return False
-
-# Print the chessboard
+   if row == n:
+       return True
+   for column in range(n):
+       if is_available(board, row, column, n):
+           board[row][column] = queen
+           if place_queens(board, row + 1, n):
+               return True
+           board[row][column] = 0
+   return False
 def print_board(board, n):
-    for i in range(n):
-        for j in range(n):
-            if (i + j) % 2 == 0:
-                print("\u25A1", end=" ")
-            else:
-                print("\u25A0", end=" ")
-        print()
-        for j in range(n):
-            if board[i][j] == queen:
-                print(queen, end=" ")
-            else:
-                print("\u2001", end=" ")
-        print()
-
-# Solve the N-Queens problem
+   for i in range(n):
+       for j in range(n):
+           if board[i][j] == queen:
+               print(f"{board[i][j]}", end=" ")
+           else:
+               print(f"\u25A1", end=" ")
+       print()
 def solve_n_queens(n):
-    board = [[0] * n for _ in range(n)]
-    if place_queens(board, 0, n) == False:
-        print("No solution exists")
-        return False
-    print_board(board, n)
+   board = [[0] * n for _ in range(n)]
+   if place_queens(board, 0, n) == False:
+       print("No solution exists")
+       return False
+   print_board(board, n)
+solve_n_queens(8)
 
-# Solve the 4-Queens problem
-solve_n_queens(4)
